3ec5626c63c2c1c74436310cab38990aedfc027b
Hanni87 <Hanni87@users.noreply.github.com>
2023-02-19 22:31:35 +0000

    Game logic

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
-function GuessResults({ guesses }) {
+function GuessResults({ guesses, answer }) {
   return (
     <div className="guess-results">
       {range(0, NUM_OF_GUESSES_ALLOWED).map((rowIndex) => {
+        const guess = guesses[rowIndex];
+        const checkedGuess = checkGuess(guess, answer);
         return (
           <p className="guess" key={rowIndex}>
+            {/* Could be game component */}
             {range(0, 5).map((colIndex) => {
+              const status = checkedGuess ? checkedGuess[colIndex].status : "";
               return (
-                <span className="cell" key={colIndex}>
-                  {guesses[rowIndex]?.charAt(colIndex)}
+                <span className={`cell ${status}`} key={colIndex}>
+                  {guess?.charAt(colIndex)}
                 </span>
               );
             })}
